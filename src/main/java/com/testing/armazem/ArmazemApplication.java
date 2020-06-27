75df16e54451dfa91d4c92baae421c83327b5b27
Denis Romano <denis.romano@gmail.com>
2020-06-27 14:41:15 -0300

    commit inicial

---
+++ src/main/java/com/testing/armazem/ArmazemApplication.java
@@ -0,0 +1,15 @@
+package com.testing.armazem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmazemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmazemApplication.class, args);
+
+
+	}
+
+}
